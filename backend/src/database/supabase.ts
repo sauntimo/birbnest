e4a3ed4eed3b520ea1f1123f1828810ec344a94a
Tim Saunders <tim@sauntimo.org>
2021-07-07 00:41:16 +0100

    Fix db init

--- backend/src/database/supabase.ts
+++ backend/src/database/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
-export const db = createClient(
+export const supabase = createClient(
   process.env.DB_URL,
   process.env.DB_PUBLIC_ANON_KEY,
 );
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
